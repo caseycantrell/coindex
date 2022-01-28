c69f4a3792367b2e6458a7712a47a8e8bb7cd8d5
caseycantrell <cantrellcasey@gmail.com>
2022-01-28 13:15:28 -0600

    adds api call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import "./App.css"
-
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 function App() {
 
-  
+  const [ coins, setCoins ] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    .then(res => {
+      setCoins(res.data)
+    }).catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         <p>Hellloooo</p>
       </header>
+      <div className="coin-search">
+        <h1 className="coin-text">Search a cryptocurrency</h1>
+      </div>
     </div>
   );
 }
